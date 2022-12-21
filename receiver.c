408ff10171e2f4958bbf1eaa85e20eeb99e16bd5
ravidkrisi <ravidkrisi22@gmail.com>
2022-12-21 18:41:55 +0200

    version 1.0

--- receiver.c
+++ receiver.c
@@ -1,112 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <sys/time.h>
+#define SIZE 2120000
 
-
-#define SIZE 1024
-
+//a func that gets a the clientsocket and prints the file it gets from the sender
 void write_file(int sockfd)
 {
-    int n;
-    char buffer[SIZE];
+int n;
+char buffer[SIZE];
 
-    while(1)
-    {
-        n=recv(sockfd, buffer, SIZE, 0);
-        
-        if(n<=0)
-        {
-            return;
-        }
-
-        printf("%s", buffer);
-        bzero(buffer, SIZE);
-    }
+n = recv(sockfd, buffer, SIZE, 0);
+// printf("%d\n",n);
+if (n <= 0)
+{
     return;
 }
+printf("%s", buffer);
+bzero(buffer, SIZE);
+}
 
-int main()
-{
-    char *ip = "127.0.0.1";
-    int port =8082;
-    int e;
+int main(){
+char *ip = "127.0.0.1";
+int port = 8082;
+int e;
 
-    int sockfd, new_sock;
-    struct sockaddr_in server_addr, new_addr;
-    socklen_t addr_size;
-    char buffer[SIZE];
+int sockfd, new_sock;
+struct sockaddr_in server_addr, new_addr;
+socklen_t addr_size;
+char buffer[SIZE];
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd<0)
-    {
-        perror("[-]Error in socket");
-        exit(1);
-    }
-    printf("[+]Server socket created successfully.\n");
+sockfd = socket(AF_INET, SOCK_STREAM, 0);
+if(sockfd < 0) {
+perror("[-]Error in socket");
+exit(1);
+}
+printf("[+]Server socket created successfully.\n");
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = port;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+server_addr.sin_family = AF_INET;
+server_addr.sin_port = port;
+server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+if(e < 0) {
+perror("[-]Error in bind");
+exit(1);
+}
+printf("[+]Binding successfull.\n");
 
-    if(e<0)
-    {
-        perror("[-]Error in bind");
-        exit(1);
-    }
-    printf("[+]binding successful.\n");
+if(listen(sockfd, 10) == 0){
+printf("[+]Listening....\n");
+}else{
+perror("[-]Error in listening");
+exit(1);
+}
 
-    if(listen(sockfd, 10)==0)
-    {
-        printf("[+]Listening....\n");
-    }
-    else
-    {
-        perror("[-]Error in listening");
-        exit(1);
-    }
+addr_size = sizeof(new_addr);
+new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
 
-    addr_size=sizeof(new_addr);
-    new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-
-    //set var to start and stop time. record time for start time
-    struct timeval stop, start;
-    gettimeofday(&start, NULL);
-    int n;
-
-    //receive part 1 of the file
-    write_file(new_sock);
-    printf("[+]Data wrriten successfully.\n");
-
-    //record time for stop time 
-    gettimeofday(&stop, NULL);
-
-    //print message of the time it took to receive the first file
-    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 10000 + stop.tv_usec - start.tv_usec);
-    
-    //send authentication
-    char pass[]="110100000001100111100110";
-    if(send(new_sock, pass, sizeof(pass), 0)==-1)
-    {
-        perror("error in sending authentication");
-    }
-
-    //record time of start time for sencond part of the file 
-    gettimeofday(&start, NULL);
-
-    //receiving the sencond part of the file 
-    write_file(new_sock);
+//gets the first part of the file 
+write_file(new_sock);
+printf("[+]Data1 written in the file successfully.\n");
 
 
-    //record stop time of the sencond part of the file 
-    gettimeofday(&stop, NULL);
+// set authentication in a string 
+// char *message = "110100000001100111100110\n";
+// int messageLen = strlen(message) + 1;
 
-    //print the time it took to receive the second part of the file
-    printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 10000 + stop.tv_usec - start.tv_usec);
+// //send the authentication to the server
+// if (send(sockfd, message, sizeof(message), 0) == -1) {
+// perror("[-]Error in sending file.");
+// exit(1);
+// }
 
-    return 0;
-}   
+//gets the second part of the file
+write_file(new_sock);
+printf("[+]Data2 written in the file successfully.\n");
+
+return 0;
+}
--- sender.c
+++ sender.c
@@ -1,67 +1,52 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <netinet/tcp.h>
+#define SIZE 2120000
 
-#define SIZE 1024
-
-//func gets a file and socket and sends it to the receiver
-void send_file(FILE *fp, int sockfd)
+//send fucntion gets file and send it to the reciever
+void send_file(char arr[SIZE], int sockfd){
+int n;
+char data[SIZE] = {0};
+// while(fgets(data, SIZE, fp) != NULL) {
+if (send(sockfd, arr, SIZE/2, 0) == -1) 
 {
-    char data[SIZE] = {0};
-
-    while(fgets(data, SIZE,fp)!=NULL)
-    {
-        if(send(sockfd, data, sizeof(data), 0) ==-1)
-        {
-            perror("[-]Error in sending file.");
-            exit(1);
-        }
-        bzero(data, SIZE);
-    }
+    perror("[-]Error in sending file.");
+    exit(1);
+}
+bzero(data, SIZE);
 }
 
-int main()
-{
-    char *ip= "127.0.0.1";
-    int port =8082;
-    int e;
 
-    int sockfd;
-    struct sockaddr_in server_addr;
-    
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
-    {
-        perror("[-]Error in socket");
-        exit(1);
-    }
-    printf("[+]Server socket created successfully.\n");
+int main(){
+char *ip = "127.0.0.1";
+int port = 8082;
+int e;
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = port;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if(e == -1)
-    {
-        perror("[-]Error in socket");
-        exit(1);
-    }
-    printf("[+]Connected to Server.\n");
+int sockfd;
+struct sockaddr_in server_addr;
 
-    //read the file
+sockfd = socket(AF_INET, SOCK_STREAM, 0);
+if(sockfd < 0) {
+perror("[-]Error in socket");
+exit(1);
+}
+printf("[+]Server socket created successfully.\n");
+
+server_addr.sin_family = AF_INET;
+server_addr.sin_port = port;
+server_addr.sin_addr.s_addr = inet_addr(ip);
+
+//split the file into 2 equal files 
     FILE *file;
     file = fopen("ex3.txt", "r");
     if(file == NULL)
     {
         printf("couldnt load the file");
     }
-
-    //split the file into 2 equal size files 
+     
     fseek(file,0,SEEK_END);
     int length = ftell(file);
     fseek(file,0,SEEK_SET);
@@ -69,62 +54,46 @@ int main()
     int length1 = length/2;
     int length2 = length-length1;
 
-    FILE *part1, *part2;
-    part1 = fopen("part1.txt", "w+");
-    part2 = fopen("part2.txt", "w+");
+    char arr[SIZE] = {'\0'};
 
     char c;
     int i =0;
     while((c=fgetc(file))!=EOF)
     {
-        if(i<=length1)
-        {
-            fprintf(part1,"%c", c);
-        }
-        else
-        {
-            fprintf(part2,"%c", c);
-        }
+        arr[i]=c;
         i++;
     }
-   
-   fseek(part1,0,SEEK_SET);
-   fseek(part2,0,SEEK_SET);
+   printf("%s", arr);
+//create a connection with the receiver
+e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+if(e == -1) {
+perror("[-]Error in socket");
+exit(1);
+}
+printf("[+]Connected to Server.\n");
 
-    //set the cc algorithm to CUBIC 
-    if (setsockopt(sockfd, IPPROTO_TCP, TCP_CONGESTION, "cubic", 5) < 0)
-    {
-      printf("set socket error from client\n");
-    }
 
-    //send the first part of the file 
-    send_file(part1, sockfd);
-    printf("[+]File data sent successfully.\n");
+//send the first part of the file
+send_file(arr, sockfd);
+printf("[+]File1 data sent successfully.\n");
 
-    ////receive the authentatication from the receiver
-    int n;
-    char buffer[SIZE];
-    while(1)
-    {
-        n=recv(sockfd, buffer, SIZE, 0);
-        
-        if(n<=0)
-        {
-            break;
-        }
+// //receive authentication from the receiver
+// char buffer[SIZE]={'\0'};
+// int n=recv(sockfd, buffer, SIZE, 0);
+// if(n<=0)
+// {
+//     printf("[+]failed to get authentication");
+//     exit(1);
+// }
+// printf("%s", buffer);
 
-        printf("%s", buffer);
-        bzero(buffer, SIZE);
-    }
 
-    // set the cc algorithm to RENO 
-    if (setsockopt(sockfd, IPPROTO_TCP, TCP_CONGESTION, "reno", 4) < 0)
-        {
-            printf("set socket error from client\n");
-        }
+//send the second part of the file 
+send_file(arr+length1, sockfd);
+printf("[+]File2 data sent successfully.\n");
 
-    printf("[+]Closing the connection.\n");
-    close(sockfd);
-    
-    return 0;
+printf("[+]Closing the connection.\n");
+close(sockfd);
+
+return 0;
 }
